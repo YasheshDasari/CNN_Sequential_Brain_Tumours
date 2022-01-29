6238fc6408a879d5c88f51352720307c20672ae1
Yashesh Dasari <ydasari@uwaterloo.ca>
2022-01-28 21:21:28 -0500

    Replaced flow_from_dir to image_dataset_from_dir. Acc 99%. Wrong prediction

--- src/machine_learning.py
+++ src/machine_learning.py
@@ -5,6 +5,8 @@ from keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 import numpy as np
 from keras.preprocessing import image
+from tensorflow.keras import layers
+from tensorflow import keras
 
 
 class Modeling:
@@ -17,7 +19,10 @@ class Modeling:
         os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
         # Model definition: Sequential CNN
+        img_h = 150
+        img_w = 150
         model = tf.keras.models.Sequential([
+            tf.keras.layers.Rescaling(1./255, input_shape=(img_h, img_w, 3)),
             tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(150, 150, 3)),
             tf.keras.layers.MaxPooling2D(2, 2),
 
@@ -28,106 +33,51 @@ class Modeling:
             tf.keras.layers.MaxPooling2D(2, 2),
 
             tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(512, activation='relu'),
+            tf.keras.layers.Dense(128, activation='relu'),
             tf.keras.layers.Dense(1, activation='sigmoid')
         ])
 
-        print("Model summary:", model.summary())
+        # print("Model summary:", model.summary())
 
         # Compiling the model
         model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
 
         training_dir = "trial1/augmented data1/training"
-        train_datagen = ImageDataGenerator(rescale=1/255)
-                                           # rotation_range=40,
-                                           # width_shift_range=0.2,
-                                           # height_shift_range=0.2,
-                                           # shear_range=0.2,
-                                           # zoom_range=0.2,
-                                           # horizontal_flip=True,
-                                           # fill_mode='nearest')
-
-        train_generator = train_datagen.flow_from_directory(training_dir, batch_size=120, classes=['no', 'yes'],
-                                                            class_mode='binary',
-                                                            target_size=(150, 150))
-
         validation_dir = "trial1/augmented data1/testing"
-        # os.path.join("trial1", "augmented data1", "testing")
 
-        validation_datagen = ImageDataGenerator(rescale=1/255)
+        train_ds = tf.keras.utils.image_dataset_from_directory(training_dir, seed=123,
+                                                               image_size=(img_h, img_w), batch_size=120)
 
-        validation_generator = validation_datagen.flow_from_directory(validation_dir, batch_size=20,
-                                                                      classes=['no', 'yes'],
-                                                                      class_mode='binary', target_size=(150, 150))
+        val_ds = tf.keras.utils.image_dataset_from_directory(validation_dir, seed=123,
+                                                             image_size=(img_h, img_w), batch_size=20)
 
-        # Fitting the model with the training dataset while also defining the validation dataset
-        history = model.fit_generator(train_generator, steps_per_epoch=8, epochs=5, verbose=1,
-                                      validation_data=validation_generator, validation_steps=8)
+        class_names = val_ds.class_names
+        print(class_names)
 
-        # acc = history.history['acc']
-        # val_acc = history.history['val_acc']
-        # loss = history.history['loss']
-        # val_loss = history.history['val_loss']
+        plt.figure(figsize=(10, 10))
+        for images, labels in train_ds.take(1):
+            for i in range(9):
+                ax = plt.subplot(3, 3, i + 1)
+                plt.imshow(images[i].numpy().astype("uint8"))
+                plt.title(class_names[labels[i]])
+                plt.axis("off")
+            plt.show()
 
-        prediction_dir = "trial1/augmented data1/prediction"
-        # for i in os.listdir(prediction_dir):
-        #     img = image.load_img(prediction_dir, target_size=(150, 150))
-        #     x = image.img_to_array(img)
-        #     plt.imshow(x/255.)
-        #     x = np.expand_dims(x, axis=0)
-        #     images = np.vstack([x])
-        #     classes = model.predict(images, batch_size=10)
-        #     print(classes[0])
-        #     if classes[0]<0.5:
-        #         print(i + "is non-tumorous")
-        #     else:
-        #         print(i + "is tumorous")
+        history = model.fit(train_ds, epochs=15, validation_data=val_ds)
 
-        test_datagen = ImageDataGenerator(rescale=1/255)
-        test_generator = test_datagen.flow_from_directory(
-            prediction_dir,
-            target_size=(150, 150),
-            batch_size=1,
-            shuffle=False,
-            class_mode=None
+        pred_dir = "trial1/augmented data1/prediction/aug_Y10_0_1877.jpg"
+
+        pred_img = tf.keras.utils.load_img(pred_dir, target_size=(img_h, img_w))
+        img_array = tf.keras.utils.img_to_array(pred_img)
+
+        img_array = tf.expand_dims(img_array, 0)  # Create a batch
+
+        predictions = model.predict(img_array)
+        print("Pred:", predictions)
+        print("Pred[0]", predictions[0])
+        score = tf.nn.softmax(predictions[0])
+
+        print(
+            "This image most likely belongs to {} with a {:.2f} percent confidence."
+            .format(class_names[np.argmax(score)], 100 * np.max(score))
         )
-
-        # probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
-        # new_predictions = probability_model.predict(test_generator)
-        # print("First element of prediction (before argmax):", new_predictions[0])
-        # np.argmax(new_predictions)
-        # print("New predictions: ", new_predictions)
-
-        # STEP_SIZE_TEST = test_generator.n // test_generator.batch_size
-        test_generator.reset()
-        pred = model.predict(test_generator)
-        # pred = model.predict_generator(test_generator,
-        #                                steps=1,
-        #                                verbose=1)
-
-        print("pred:", pred)
-        predicted_class_indices = np.argmax(pred, axis=1)
-
-        labels = train_generator.class_indices
-        print("Labels: ", labels)
-        labels = dict((v, k) for k, v in labels.items())
-        print("Second labels:", labels)
-        predictions = [labels[k] for k in predicted_class_indices]
-        #
-        filenames = test_generator.filenames
-        #
-        print(filenames, predictions, "\n")
-
-        # # Plotting the model accuracy
-        # epochs = range(len(acc))  # Get number of epochs
-        #
-        # plt.plot(epochs, acc, 'r', "Training Accuracy")
-        # plt.plot(epochs, val_acc, 'b', "Validation Accuracy")
-        # plt.title('Training and validation accuracy')
-        # plt.figure()
-        #
-        # plt.plot(epochs, loss, 'r', "Training Loss")
-        # plt.plot(epochs, val_loss, 'b', "Validation Loss")
-        #
-        # plt.title('Training and validation loss')
-        # plt.show()
